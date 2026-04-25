ec5cfeb3e1a72e97c17d86e9d05d3d297e5adc5a
lucas-foquim-a22406944 <foquimlucas@gmail.com>
2026-04-25 20:56:20 +0000

    Ficha 7

--- escolaOnline/urls.py
+++ escolaOnline/urls.py
@@ -1,9 +1,10 @@
-## escola/urls.py
-
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('cursos/', views.cursos_view, name="cursos"),
-    path('', views.cursos_view),   #  rota que abre diretamente a página dos cursos
+    path('', views.cursos_view),
+    path('cursos/', views.cursos_view, name='cursos'),
+    path('curso/<int:id>', views.curso_view, name='curso'),
+    path('professores/', views.professores_view, name='professores'),
+    path('alunos/', views.alunos_view, name='alunos'),
 ]
--- escolaOnline/views.py
+++ escolaOnline/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
-from .models import Curso
+from .models import Curso, Professor, Aluno
 
 def cursos_view(request):
-
     cursos = Curso.objects.select_related('professor').prefetch_related('alunos').all()
-    
     return render(request, 'escolaOnline/cursos.html', {'cursos': cursos})
+
+def curso_view(request, id):
+    curso = Curso.objects.get(id=id)
+    return render(request, 'escolaOnline/curso.html', {'curso': curso})
+
+def professores_view(request):
+    professores = Professor.objects.prefetch_related('cursos').all()
+    return render(request, 'escolaOnline/professores.html', {'professores': professores})
+
+def alunos_view(request):
+    alunos = Aluno.objects.prefetch_related('cursos').all()
+    return render(request, 'escolaOnline/alunos.html', {'alunos': alunos})
